6b6c0880731e687d10fc3acc3b2150110e7cbaf6
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2023-11-03 23:01:37 +0530

    added car model to backend and test1.html

--- cars/carshowroom/models.py
+++ cars/carshowroom/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # Add other fields for car details
