bb7aed5c79380ef08b9c5d347637b7aa5712d24c
DHRUV THAKUR <145418886+DhruvTh2001@users.noreply.github.com>
2023-11-15 08:09:56 +0530

    updated addcar request for backend
    this will enable the data flow from front end to backend for add car page

--- cars/carshowroom/views.py
+++ cars/carshowroom/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 def admindashboard(request):
     return render(request,'admindashboard.html')
 
-    
+def addcar(request):
+     if request.method=='POST':
+        make=request.POST.get('make')
+        model=request.POST.get('model')
+        year=request.POST.get('year')
+        price=request.POST.get('price')
+        car=Car(make=make,model=model,year=year,price=price)
+        car.save()
