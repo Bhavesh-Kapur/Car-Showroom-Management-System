8d94550b02b0b0ecb033dbf5d4ccece909d47289
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2023-11-15 10:36:16 +0530

    new model created customers

--- cars/carshowroom/models.py
+++ cars/carshowroom/models.py
@@ -11,3 +11,9 @@ class Booking(models.Model):
     client_name = models.CharField(max_length=255)
     booking_date = models.DateField()
     rate=models.DecimalField(max_digits=10, decimal_places=2)
+
+class Customer(models.Model):
+    name=models.CharField(max_length=100)
+    phone=models.IntegerField()
+    email=models.CharField(max_length=50)
+    address=models.CharField(max_length=200)
