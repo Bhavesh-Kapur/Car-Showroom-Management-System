5541d4b126ab95451ae5f016181a498bfa89fb17
PrinceV007 <143865246+PrinceV007@users.noreply.github.com>
2023-11-15 09:50:50 +0530

    updated views for add car input fields

--- cars/carshowroom/views.py
+++ cars/carshowroom/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from carshowroom.models import Car
 
 # Create your views here.
 def admindashboard(request):
     return render(request,'admindashboard.html')
 
 def addcar(request):
-     if request.method=='POST':
+    if request.method=='POST':
         make=request.POST.get('make')
         model=request.POST.get('model')
         year=request.POST.get('year')
         price=request.POST.get('price')
         car=Car(make=make,model=model,year=year,price=price)
         car.save()
+    return render(request,'addcar.html')
