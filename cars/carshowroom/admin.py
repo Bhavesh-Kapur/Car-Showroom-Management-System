33910872668b68a3993102b9234c9143be177a1b
PrinceV007 <143865246+PrinceV007@users.noreply.github.com>
2023-11-19 10:47:18 +0530

    Update admin.py for different models

--- cars/carshowroom/admin.py
+++ cars/carshowroom/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car,Customer,Booking,Query
 
 admin.site.register(Car)
+admin.site.register(Custome)
+admin.site.register(Booking)
+admin.site.register(Query)
